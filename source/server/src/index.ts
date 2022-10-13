70ad33e79596248daab4e70493e936b040eb9e84
Dylan Archer <dylarcher@gmail.com>
2022-10-13 12:29:15 -0400

    cleans up server/index.ts a smidge more

--- source/server/src/index.ts
+++ source/server/src/index.ts
@@ -3,15 +3,14 @@ import cors from 'cors';
 import express from 'express';
 import { join } from 'path';
 
-const app = express();
-
-app.use(cors());
-app.use(express.static(join(__dirname, '../../app/dist')));
-
-app.get('*', (req: any, resources: any) => {
-  resources.sendFile(join(__dirname, '../../app/dist', 'index.html'));
-});
-
-app.listen(__appdata.port, () => {
-  console.log(`${__appdata.title}'s server listening at http://localhost:${__appdata.port}`);
-});
+express()
+  .use(cors())
+  .use(express.static(join(__dirname, '../../app/dist')))
+  .get('*', (req: any, resources: any) =>
+    resources.sendFile(join(__dirname, '../../app/dist', 'index.html')),
+  )
+  .listen(__appdata.port, () =>
+    console.log(
+      `${__appdata.title}'s server listening at http://localhost:${__appdata.port}`,
+    ),
+  );
