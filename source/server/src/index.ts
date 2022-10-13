5702bc2e22691cb8ee349fee87d4fb12e2b43958
Dylan Archer <dylarcher@gmail.com>
2022-10-13 12:26:21 -0400

    cleans up server index file

--- source/server/src/index.ts
+++ source/server/src/index.ts
@@ -3,15 +3,11 @@ import cors from 'cors';
 import express from 'express';
 import { join } from 'path';
 
-const PORT = 3000;
-
 const app = express();
-app.use(cors());
 
-// Serve static resources from the "public" folder (ex: when there are images to display)
+app.use(cors());
 app.use(express.static(join(__dirname, '../../app/dist')));
 
-// Serve the HTML page
 app.get('*', (req: any, resources: any) => {
   resources.sendFile(join(__dirname, '../../app/dist', 'index.html'));
 });
